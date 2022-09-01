ef9b194f4523ce81f7c43d76bd84f2b3dcc874e6
Andrey Pelagio <andreypfn123@hotmail.com>
2022-09-01 17:40:03 -0300

    pronto4

---
+++ Notas.py
@@ -0,0 +1,6 @@
+media1=float(input('Digite sua primeira nota: '))
+media2=float(input('Digite sua segunda nota: '))
+media3=float(input('Digite sua terceira nota: '))
+media4=float(input('Digite sua quarta nota: '))
+media=(media1+media2+media3+media4)/4
+print('A sua média é: ',media)
