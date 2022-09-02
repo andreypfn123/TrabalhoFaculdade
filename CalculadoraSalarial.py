cde53f9a5ea5b0685c2bcb94b590405d2fb0af40
Andrey Pelagio <andreypfn123@hotmail.com>
2022-09-02 00:00:02 -0300

    Pronto 8

---
+++ CalculadoraSalarial.py
@@ -0,0 +1,4 @@
+Salari_Hora=float(input('Insira quanto você ganha por hora:R$ '))
+Hora_Mes=float(input('Insita quanto em hora você trabalha no mês: '))
+Salario_Total_Mes=Salari_Hora*Hora_Mes
+print('Seu salário mensal é R${}'.format(Salario_Total_Mes))
