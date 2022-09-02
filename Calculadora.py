afbf35bcc5afe4fc175c10049399d87abf9705e0
Andrey Pelagio <andreypfn123@hotmail.com>
2022-09-02 15:51:35 -0300

    Pronto 3

--- Calculadora.py
+++ Calculadora.py
@@ -1,4 +1,4 @@
 num1 = int(input("Digite um valor: "))
 num2 = int(input("Digite outro valor: "))
 s = num1 + num2
-print("A2 soma entre {} e {} é igual a {}!".format(num1, num2, s))
+print("A soma entre {} e {} é igual a {}!".format(num1, num2, s))
