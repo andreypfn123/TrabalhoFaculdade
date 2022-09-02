8ead1f2abeb2556771db1aab3dbfe9060bd10065
Andrey Pelagio <andreypfn123@hotmail.com>
2022-09-02 15:50:25 -0300

    Pronto 11

---
+++ Calc_Real_Int.py
@@ -0,0 +1,10 @@
+from math import prod
+from re import A
+
+
+num1=int(input('Digite o primeiro número inteiro: '))
+num2=int(input('Digite o segundo número inteiro: '))
+num3=float(input('Digite o número real: '))
+print('O produto do dobro do primeiro com a metade do segundo: ',(num1*2)*(num2/2))
+print('Soma do triplo do primeiro com o terceiro: ',(num1*3)+(num3))
+print('O terceiro elevado ao cubo: ', (num3**3))
