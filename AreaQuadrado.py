84122ea9b8e49d988b3b62bfd6d112ae185abe24
Andrey Pelagio <andreypfn123@hotmail.com>
2022-09-01 23:29:04 -0300

    Pronto 7

---
+++ AreaQuadrado.py
@@ -0,0 +1,4 @@
+altura=float(input('Digite a altura do quadrado em metros: '))
+largura=float(input('Digite a largura do quadrado em metros: '))
+a=altura*largura
+print('O dobro da área é ' , a*2,'m²')
