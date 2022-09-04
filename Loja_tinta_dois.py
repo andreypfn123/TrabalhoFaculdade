d99d2b6216c5642ca3ff3566e951f3f36bf71c9f
Andrey Pelagio <andreypfn123@hotmail.com>
2022-09-04 19:59:57 -0300

    Quase acabando

--- Loja_tinta_dois.py
+++ Loja_tinta_dois.py
@@ -23,3 +23,5 @@ if galao_litros_pintar%galao_tinta !=0:
     galoes_necessarios+=1
 valor_final_galao=galoes_necessarios*25
 print('Se for apenas galões, precisará comprar',galoes_necessarios,'galões e custará R$%.2f'%valor_final_galao)
+
+# Mistura
